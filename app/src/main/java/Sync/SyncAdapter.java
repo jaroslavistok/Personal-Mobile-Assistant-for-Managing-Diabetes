44c869128e0b85ba90f01caf41d253898705cdae
Jaroslav Ištok <jaroslav.istok@gmail.com>
2016-10-28 19:30:57 +0200

    Added SyncAdapter class

---
+++ app/src/main/java/Sync/SyncAdapter.java
@@ -0,0 +1,25 @@
+package Sync;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    private ContentResolver mContentResolver;
+
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        mContentResolver = context.getContentResolver();
+    }
+
+
+    @Override
+    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
+
+    }
+}
